93d612d502201bd7d599b6a721a5fcff64f9625d
gyumin <gyumin@gyuminui-MacBookPro.local>
2020-09-28 18:25:20 +0900

    add safety_manager list

---
+++ SmartVestProject/app/src/main/java/com/example/smartvest/ManagerLocationActivity.java
@@ -0,0 +1,13 @@
+package com.example.smartvest;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ManagerLocationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
