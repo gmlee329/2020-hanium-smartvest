8e93fd0659dfb3efe7beb019c07c774e6a1be92c
gmlee329 <gmlee329@naver.com>
2020-10-14 02:28:50 +0900

    add safety worker

--- SmartVestProject/app/src/main/java/com/example/smartvest/WorkerSafetyActivity.java
+++ SmartVestProject/app/src/main/java/com/example/smartvest/WorkerSafetyActivity.java
@@ -1,18 +1,29 @@
 package com.example.smartvest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import org.w3c.dom.Text;
 
 public class WorkerSafetyActivity extends AppCompatActivity {
     ImageView back_safety_worker;
+    ConstraintLayout safety_mq5;
+    TextView text_safety_mq5;
+    ImageView safety_mq5_info;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_safety_worker);
+        safety_mq5 = findViewById(R.id.safety_mq5);
+        text_safety_mq5 = findViewById(R.id.text_safety_mq5);
+        safety_mq5_info = findViewById(R.id.safety_mq5_info);
         back_safety_worker = findViewById(R.id.back_safety_worker);
         back_safety_worker.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -20,5 +31,21 @@ public class WorkerSafetyActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        safety_mq5_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popUp(text_safety_mq5, R.raw.safety_mq5_info);
+            }
+        });
+    }
+
+    private void popUp(TextView textView, int resId) {
+        String title = textView.getText().toString();
+        String content = new MyTextReader().readTextFile(getApplicationContext(), resId);
+        Intent intent = new Intent(getApplicationContext(), PopupActivity.class);
+        intent.putExtra("title", title);
+        intent.putExtra("content", content);
+        startActivityForResult(intent, 1);
     }
 }
