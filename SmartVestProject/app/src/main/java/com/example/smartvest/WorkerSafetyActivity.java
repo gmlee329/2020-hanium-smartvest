ac8ce7ba542db602136cf5b1ff066d96206dbb97
gmlee329 <gmlee329@naver.com>
2020-10-16 03:49:57 +0900

    add graph

--- SmartVestProject/app/src/main/java/com/example/smartvest/WorkerSafetyActivity.java
+++ SmartVestProject/app/src/main/java/com/example/smartvest/WorkerSafetyActivity.java
@@ -1,6 +1,5 @@
 package com.example.smartvest;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -9,7 +8,10 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.DialogFragment;
+import androidx.core.content.ContextCompat;
+
+import com.dinuscxj.progressbar.CircleProgressBar;
+
 
 public class WorkerSafetyActivity extends AppCompatActivity {
     ImageView back_safety_worker;
@@ -22,6 +24,8 @@ public class WorkerSafetyActivity extends AppCompatActivity {
     ConstraintLayout safety_temp;
     TextView text_safety_temp;
     ImageView safety_temp_info;
+
+    CircleProgressBar graph_temp;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,8 +39,41 @@ public class WorkerSafetyActivity extends AppCompatActivity {
         safety_temp = findViewById(R.id.safety_temp);
         text_safety_temp = findViewById(R.id.text_safety_temp);
         safety_temp_info = findViewById(R.id.safety_temp_info);
-
         back_safety_worker = findViewById(R.id.back_safety_worker);
+
+        graph_temp = findViewById(R.id.graph_temp);
+        CircleProgressBar.ProgressFormatter progressFormatter = new CircleProgressBar.ProgressFormatter() {
+            @Override
+            public CharSequence format(int progress, int max) {
+                return "좋음";
+            }
+        };
+        graph_temp.setProgressFormatter(progressFormatter);
+        graph_temp.setProgressTextColor(ContextCompat.getColor(this, R.color.button_color));
+        graph_temp.setProgressBackgroundColor(ContextCompat.getColor(this, R.color.white_gray_color));
+        graph_temp.setProgressStartColor(ContextCompat.getColor(this, R.color.button_color));
+        graph_temp.setProgressEndColor(ContextCompat.getColor(this, R.color.button_color));
+        int max = 100;
+        int progress = 70;
+        int pi = 360;
+        int start = 270;
+        int degree = start-(int)((progress/(double)max)*pi);
+        graph_temp.setMax(max);
+        graph_temp.setProgress(progress);
+        graph_temp.setStartDegree(degree);
+        safety_temp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int max = 100;
+                int progress = (int)(Math.random()*101);
+                int pi = 360;
+                int start = 270;
+                int degree = start-(int)((progress/(double)max)*pi);
+                graph_temp.setProgress(progress);
+                graph_temp.setStartDegree(degree);
+            }
+        });
+
         back_safety_worker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
